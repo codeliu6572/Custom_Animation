999e69539fce4309198b4e8f851c135dd9160042
codeliu6572 <codeliu6572@163.com>
2016-06-28 14:17:46 +0800

    Initial Commit

---
+++ 自定义过渡动画/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义过渡动画
+//
+//  Created by 刘浩浩 on 16/6/28.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
